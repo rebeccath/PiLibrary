18f9eeb5f77469a321ce4828e188320459019f55
theurrbc <rebecca.theurer@online.de>
2026-02-18 10:17:52 +0100

    Kommentare

--- buecher-uebersicht.php
+++ buecher-uebersicht.php
@@ -10,7 +10,6 @@
         <input type="text" name="suche" value="Suche...">
     <br>
     <table border="1" style="border-collapse: collapse;">
-        
     <br>
     <tr>
     <th>ISBN</th>
@@ -20,29 +19,32 @@
     <th>Erscheinungsjahr</th>
     <th>Verlag</th>
     <th>Status</th>
-
     </tr>
 
     
 <?php
+//Aktiverit Fehleranzeige
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+//Optionen fpr PDO-funktion
 $options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION, // wirft Exceptions bei DB‑Fehlern
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,  // fetch() gibt Arrays ohne Indexzahlen zurück
 ];
 
+//Erstellt DPO Objekt
 $pdo = new PDO('mysql:host=localhost;dbname=PiLibrary', 'data-admin', 'passwort', $options);
 
+//Zeigt Inhalte der Tabelle "Buecher" als grafische Tabelle an
 $data = $pdo->query("SELECT * FROM Buecher");
 while ($row = $data->fetch()) {
     echo "<tr><td>". $row['ISBN'] ."</td><td>". $row['Name'] ."</td><td>". $row['Autor'] ."</td><td>". $row['Auflage'] ."</td><td>". $row['Erscheinungsjahr'] ."</td><td>". $row['Verlag'] ."</td><td>". $row['Ausleihstatus'] ."</td></tr>";
 }
-
 ?>
 
-    </table>
 
+    </table>
     </body>
 </html>
